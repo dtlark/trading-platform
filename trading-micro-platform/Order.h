db3bc959b4877168c22264bcfff6ad6c88c0fece
Deanna Lark <48802382+dtlark@users.noreply.github.com>
2022-06-07 23:22:51 -0400

    orders

--- trading-micro-platform/Order.h
+++ trading-micro-platform/Order.h
@@ -1,17 +1,23 @@
+#include <chrono>
+
 class Order {
 
+    int id;
+    bool _side;
+    float _price;
     uint _currentQuantity;
     uint _initialQuantity;
-    float _price;
-    bool _buy;
+    chrono::system_clock::time_point time;
 
 public:
-    Order(uint currentQuantity, uint initialQuantity, float price, bool buy);
+    Order(uint currentQuantity, uint initialQuantity, float price, bool side) {
+        time = chrono::system_clock::now();
+    };
 
     uint getCurrentQuantity();
     uint getInitialQuantity();
     float getPrice();
-    bool getType();
+    bool getSide();
 
     void updateQuantity(uint quantityDelta);
 }
--- trading-micro-platform/Orderbook.h
+++ trading-micro-platform/Orderbook.h
@@ -1,5 +1,21 @@
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
 class Orderbook {
+    map<string, vector<string>> orders;
+    vector<string> bids;
+    vector<string> asks;
 
+    Order() {
+        
+    }
 
+    void MarketOrder() {};
+    void LimitOrder() {};
+    void CancelOrder() {};
+    void OrderStatus();
 
 }
